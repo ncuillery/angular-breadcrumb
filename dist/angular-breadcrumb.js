87047b0989eefb3bada76f3307b3056155d818a4
Nicolas Cuillery <n.cuillery@serelec.fr>
2013-10-23 11:03:54 +0200

    Compiled version of the lastest version

--- dist/angular-breadcrumb.js
+++ dist/angular-breadcrumb.js
@@ -1,12 +1,65 @@
-/*! angular-breadcrumb - v0.1.0 - 2013-09-06
+/*! angular-breadcrumb - v0.1.0 - 2013-10-23
 * https://github.com/ncuillery/angular-breadcrumb
 * Copyright (c) 2013 Nicolas Cuillery; Licensed MIT */
-(function(exports) {
+angular.module('ncy-angular-breadcrumb', ['ui.router.state'])
+    .provider('$breadcrumb', function() {
 
-  'use strict';
+        var options = {};
 
-  exports.awesome = function() {
-    return 'awesome';
-  };
+        this.setPrefixState = function(prefixStateName) {
+            options.prefixStateName = prefixStateName;
+        };
 
-}(typeof exports === 'object' && exports || this));
+        var _pushNonexistentState = function(array, state) {
+            var stateAlreadyInArray = false;
+            angular.forEach(array, function(value) {
+                if(!stateAlreadyInArray && angular.equals(value, state)) {
+                    stateAlreadyInArray = true;
+                }
+            });
+            if(!stateAlreadyInArray) {
+                array.push(state);
+            }
+            return stateAlreadyInArray;
+        };
+
+        this.$get = ['$state', function($state) {
+
+            return {
+                getStatesChain : function() {
+                    var chain = [];
+
+                    // Prefix state
+                    if(options.prefixStateName) {
+                        var prefixState = $state.get(options.prefixStateName);
+                        if(prefixState) {
+                            _pushNonexistentState(chain, prefixState);
+                        } else {
+                            throw 'Bad configuration : prefixState "' + options.prefixStateName + '" unknown';
+                        }
+                    }
+
+                    angular.forEach($state.$current.path, function(value) {
+                        _pushNonexistentState(chain, value.self);
+                    });
+
+                    return chain;
+                }
+            };
+        }];
+
+    })
+    .directive('ncyBreadcrumb', function($state, $breadcrumb) {
+        return function(scope, element) {
+
+            scope.$watch(function() { return $state.current; }, function() {
+                var chain = $breadcrumb.getStatesChain();
+                var stateNames = [];
+                angular.forEach(chain, function(value) {
+                    stateNames.push(value.name);
+                });
+                element.text(stateNames.join(' / '));
+            }, true);
+
+        };
+    });
