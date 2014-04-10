f4bb61b8b2aab01c2dd24fba9a7e9e1887eda834
Nicolas Cuillery <n.cuillery@serelec.fr>
2014-04-10 18:14:02 +0200

    New version with templating

--- sample/asset/angular-breadcrumb.js
+++ sample/asset/angular-breadcrumb.js
@@ -1,16 +1,20 @@
-/*! angular-breadcrumb - v0.1.0 - 2014-04-09
- * https://github.com/ncuillery/angular-breadcrumb
- * Copyright (c) 2014 Nicolas Cuillery; Licensed MIT */
+/*! angular-breadcrumb - v0.1.0 - 2014-04-10
+* https://github.com/ncuillery/angular-breadcrumb
+* Copyright (c) 2014 Nicolas Cuillery; Licensed MIT */
 angular.module('ncy-angular-breadcrumb', ['ui.router.state'])
     .provider('$breadcrumb', function() {
 
-        var options = {};
-
-        this.setPrefixState = function(prefixStateName) {
-            options.prefixStateName = prefixStateName;
+        var $$options = {
+            prefixStateName: null,
+            template: 'bootstrap3',
+            templateUrl: null
         };
 
-        var _pushNonexistentState = function(array, state) {
+        this.setOptions = function(options) {
+            angular.extend($$options, options);
+        };
+
+        var $pushNonexistentState = function(array, state) {
             var stateAlreadyInArray = false;
             angular.forEach(array, function(value) {
                 if(!stateAlreadyInArray && angular.equals(value, state)) {
@@ -26,23 +30,37 @@ angular.module('ncy-angular-breadcrumb', ['ui.router.state'])
         this.$get = ['$state', function($state) {
 
             return {
-                getStatesChain : function() {
+                getTemplate: function(templates) {
+                    if($$options.templateUrl) {
+                        // templateUrl takes precedence over template
+                        return null;
+                    } else if(templates[$$options.template]) {
+                        // Predefined templates (bootstrap, ...)
+                        return templates[$$options.template];
+                    } else {
+                        return $$options.template;
+                    }
+                },
+                getTemplateUrl: function() {
+                    return $$options.templateUrl;
+                },
+                getStatesChain: function() {
                     var chain = [];
 
-                    // Prefix state
-                    if(options.prefixStateName) {
-                        var prefixState = $state.get(options.prefixStateName);
+                    // Prefix state treatment
+                    if($$options.prefixStateName) {
+                        var prefixState = $state.get($$options.prefixStateName);
                         if(prefixState) {
                             var prefixStep = angular.extend(prefixState, {ncyBreadcrumbLink: $state.href(prefixState)});
-                            _pushNonexistentState(chain, prefixStep);
+                            $pushNonexistentState(chain, prefixStep);
                         } else {
-                            throw 'Bad configuration : prefixState "' + options.prefixStateName + '" unknown';
+                            throw 'Bad configuration : prefixState "' + $$options.prefixStateName + '" unknown';
                         }
                     }
 
                     angular.forEach($state.$current.path, function(value) {
                         var step = angular.extend(value.self, {ncyBreadcrumbLink: $state.href(value)});
-                        _pushNonexistentState(chain, step);
+                        $pushNonexistentState(chain, step);
                     });
 
                     return chain;
@@ -52,19 +70,31 @@ angular.module('ncy-angular-breadcrumb', ['ui.router.state'])
 
     })
     .directive('ncyBreadcrumb', ['$state', '$interpolate', '$breadcrumb', '$rootScope', function ($state, $interpolate, $breadcrumb, $rootScope) {
+        this.$$templates = {
+            bootstrap2: '<ul class="breadcrumb">' +
+                    '<li ng-repeat="step in steps | limitTo:(steps.length-1)">' +
+                        '<a href="{{step.ncyBreadcrumbLink}}">{{step.ncyBreadcrumbLabel}}</a> ' +
+                        '<span class="divider">/</span>' +
+                    '</li>' +
+                    '<li ng-repeat="step in steps | limitTo:-1" class="active">' +
+                        '<span>{{step.ncyBreadcrumbLabel}}</span>' +
+                    '</li>' +
+                '</ul>',
+            bootstrap3: '<ol class="breadcrumb">' +
+                    '<li ng-repeat="step in steps | limitTo:(steps.length-1)">' +
+                        '<a href="{{step.ncyBreadcrumbLink}}">{{step.ncyBreadcrumbLabel}}</a> ' +
+                    '</li>' +
+                    '<li ng-repeat="step in steps | limitTo:-1" class="active">' +
+                        '<span>{{step.ncyBreadcrumbLabel}}</span>' +
+                    '</li>' +
+                '</ol>'
+        };
+
         return {
             replace: true,
             scope: {},
-            template:
-                '<ul class="breadcrumb">' +
-                '<li ng-repeat="step in steps | limitTo:(steps.length-1)">' +
-                '<a href="{{step.ncyBreadcrumbLink}}">{{step.ncyBreadcrumbLabel}}</a> ' +
-                '<span class="divider">/</span>' +
-                '</li>' +
-                '<li ng-repeat="step in steps | limitTo:-1" class="active">' +
-                '<span>{{step.ncyBreadcrumbLabel}}</span>' +
-                '</li>' +
-                '</ul>',
+            template: $breadcrumb.getTemplate(this.$$templates),
+            templateUrl: $breadcrumb.getTemplateUrl(),
             link: {
                 post: function postLink(scope) {
                     $rootScope.$on('$viewContentLoaded', function (event) {
@@ -82,3 +112,4 @@ angular.module('ncy-angular-breadcrumb', ['ui.router.state'])
             }
         };
     }]);
+
