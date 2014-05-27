1278b22acf5d33580505d2d526a03a4e1add60cd
ncuillery <nicolas.cuillery@gmail.com>
2014-05-27 13:36:36 +0200

    chore(sample): give the sample a website looking
    The sample hosted on gh-pages can be considered as the official website of the module.

--- sample/app.js
+++ sample/app.js
@@ -7,45 +7,20 @@ angular.module('ncy-sample', ['ui.router.state', 'ui.bootstrap', 'ncy-angular-br
       template: 'bootstrap2'
     });
   })
-  .value('rooms', [
-    {roomId: 1, roomNumber: 101, type: 'Double'},
-    {roomId: 2, roomNumber: 102, type: 'Double'},
-    {roomId: 3, roomNumber: 103, type: 'Single'},
-    {roomId: 4, roomNumber: 104, type: 'Double'}
-  ])
-  .factory('dateUtils', function() {
-    return {
-      addDays: function(days, date) {
-        if(!date) {
-          var todayTime = new Date();
-          todayTime.setHours(0,0,0,0);
-          date = new Date(todayTime);
-        }
-
-        var newDate = new Date(date);
-        newDate.setDate(date.getDate() + days);
-        return newDate;
-        }
-    }
-  })
-  .factory('reservations', function(dateUtils) {
-    return [
-      {reservationId: 1, guestName: 'Robert Smith', roomId: '2', from: dateUtils.addDays(-1), nights: 3},
-      {reservationId: 2, guestName: 'John Doe', roomId: '3', from: dateUtils.addDays(-8), nights: 5},
-      {reservationId: 3, guestName: 'William Gordon', roomId: '1', from: dateUtils.addDays(3), nights: 6},
-      {reservationId: 4, guestName: 'Michael Robinson', roomId: '2', from: dateUtils.addDays(6), nights: 2},
-      {reservationId: 5, guestName: 'Tracy Marschall', roomId: '3', from: dateUtils.addDays(12), nights: 1}
-    ];
-  })
   .config(function($stateProvider, $urlRouterProvider) {
-
     $stateProvider
       .state('home', {
         url: '/home',
         templateUrl: 'views/home.html',
-        controller: 'HomeCtrl',
         data: {
           ncyBreadcrumbLabel: 'Home'
+        }
+      })
+      .state('sample', {
+        url: '/sample',
+        templateUrl: 'views/sample.html',
+        data: {
+          ncyBreadcrumbLabel: 'Sample'
         }
       })
       .state('booking', {
@@ -53,7 +28,8 @@ angular.module('ncy-sample', ['ui.router.state', 'ui.bootstrap', 'ncy-angular-br
         templateUrl: 'views/booking_list.html',
         controller: 'BookingListCtrl',
         data: {
-          ncyBreadcrumbLabel: 'Reservations'
+          ncyBreadcrumbLabel: 'Reservations',
+          ncyBreadcrumbParent: 'sample'
         }
       })
       .state('booking.day', {
@@ -85,7 +61,8 @@ angular.module('ncy-sample', ['ui.router.state', 'ui.bootstrap', 'ncy-angular-br
         templateUrl: 'views/room_list.html',
         controller: 'RoomListCtrl',
         data: {
-          ncyBreadcrumbLabel: 'Rooms'
+          ncyBreadcrumbLabel: 'Rooms',
+          ncyBreadcrumbParent: 'sample'
         }
       })
       .state('room.detail', {
@@ -115,4 +92,39 @@ angular.module('ncy-sample', ['ui.router.state', 'ui.bootstrap', 'ncy-angular-br
 
     $urlRouterProvider.otherwise('/home');
 
+  })
+  .value('rooms', [
+    {roomId: 1, roomNumber: 101, type: 'Double'},
+    {roomId: 2, roomNumber: 102, type: 'Double'},
+    {roomId: 3, roomNumber: 103, type: 'Single'},
+    {roomId: 4, roomNumber: 104, type: 'Double'}
+  ])
+  .factory('reservations', function(dateUtils) {
+    return [
+      {reservationId: 1, guestName: 'Robert Smith', roomId: '2', from: dateUtils.addDays(-1), nights: 3},
+      {reservationId: 2, guestName: 'John Doe', roomId: '3', from: dateUtils.addDays(-8), nights: 5},
+      {reservationId: 3, guestName: 'William Gordon', roomId: '1', from: dateUtils.addDays(3), nights: 6},
+      {reservationId: 4, guestName: 'Michael Robinson', roomId: '2', from: dateUtils.addDays(6), nights: 2},
+      {reservationId: 5, guestName: 'Tracy Marschall', roomId: '3', from: dateUtils.addDays(12), nights: 1}
+    ];
+  })
+  .factory('dateUtils', function() {
+    return {
+      addDays: function(days, date) {
+        if(!date) {
+          var todayTime = new Date();
+          todayTime.setHours(0,0,0,0);
+          date = new Date(todayTime);
+        }
+
+        var newDate = new Date(date);
+        newDate.setDate(date.getDate() + days);
+        return newDate;
+      }
+    }
+  })
+  .run(function($rootScope, $state) {
+    $rootScope.isActive = function(stateName) {
+      return $state.includes(stateName);
+    }
   });
