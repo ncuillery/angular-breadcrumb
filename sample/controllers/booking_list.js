ncuillery/angular-breadcrumb

commit 646f70607e494f0e5e3c2483ed69f689684b2742

fix(sample): use the same variable in the datepicker and from url params for state `booking.day`
In state `booking`, the model for the datepicker was not associate to the date of state `booking.day`. In case of direct access to this state by URL, the datepicker was not initialized.

Closes #16

sample/controllers/booking_list.js
@@ -4,7 +4,7 @@ angular.module('ncy-sample')
     // Some hardcoded data ;
     $scope.reservations = reservations;
 
-    $scope.$watch('reservationFilter', function(newValue) {
+    $scope.$watch('reservationDate', function(newValue) {
       if(newValue) {
         $state.go('booking.day', {year: newValue.getFullYear(), month: newValue.getMonth() + 1, day: newValue.getDate()});
       }

sample/views/booking_list.html
@@ -6,9 +6,7 @@
 
 <div class="row">
     <div class="span6">
-        <div ng-model="reservationFilter">
-            <datepicker date-disabled="!between(date).length" />
-        </div>
+        <datepicker date-disabled="!between(date).length" ng-model="reservationDate" />
     </div>
     <div class="span6" ui-view>
 

sample/app.js
@@ -36,6 +36,9 @@ angular.module('ncy-sample', ['ui.router.state', 'ui.bootstrap', 'ncy-angular-br
         url: '/:year-:month-:day',
         templateUrl: 'views/booking_day.html',
         controller: 'BookingDayCtrl',
+        onExit: function($rootScope) {
+          $rootScope.reservationDate = undefined;
+        },
         data: {
           ncyBreadcrumbLabel: 'Reservations for {{reservationDate | date:\'mediumDate\'}}'
         }

sample/controllers/booking_day.js
@@ -1,6 +1,6 @@
 angular.module('ncy-sample')
-  .controller('BookingDayCtrl', function($scope, $stateParams, rooms) {
-    $scope.reservationDate = new Date($stateParams.year, $stateParams.month - 1, $stateParams.day);
+  .controller('BookingDayCtrl', function($scope, $rootScope, $stateParams, rooms) {
+    $rootScope.reservationDate = new Date($stateParams.year, $stateParams.month - 1, $stateParams.day);
 
     $scope.getRoom = function(id) {
         return _.findWhere(rooms, {roomId: parseInt(id)});

commit 6aab00c1dc20711ad2b034e7786286e85ea83843

fix(sample): fix a regression born from commit 646f706

sample/controllers/booking_list.js
@@ -1,11 +1,17 @@
 angular.module('ncy-sample')
-  .controller('BookingListCtrl', function($scope, $state, dateUtils, reservations) {
+  .controller('BookingListCtrl', function($scope, $rootScope, $state, dateUtils, reservations) {
 
     // Some hardcoded data ;
     $scope.reservations = reservations;
 
-    $scope.$watch('reservationDate', function(newValue) {
-      if(newValue) {
+    $scope.$watch('reservationDate', function(newValue, oldValue) {
+        $scope.dpModel = $rootScope.reservationDate;
+    });
+
+
+    $scope.$watch('dpModel', function(newValue, oldValue) {
+      console.log(oldValue, '--->', newValue);
+      if(newValue && !angular.equals(newValue, oldValue)) {
         $state.go('booking.day', {year: newValue.getFullYear(), month: newValue.getMonth() + 1, day: newValue.getDate()});
       }
     });

sample/views/booking_list.html
@@ -6,7 +6,7 @@
 
 <div class="row">
     <div class="span6">
-        <datepicker date-disabled="!between(date).length" ng-model="reservationDate" />
+        <datepicker date-disabled="!between(date).length" ng-model="dpModel" />
     </div>
     <div class="span6" ui-view>
 