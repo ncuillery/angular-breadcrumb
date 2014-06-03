6aa8ed2facb500e2373d0d225f3a19d4f816f63d
ncuillery <nicolas.cuillery@gmail.com>
2014-06-03 20:16:15 +0200

    fix(sample): make the CRU(D) about rooms working
    "New" and "Edit" buttons are working now. Modifications are effective until the next refresh.

--- sample/controllers/room_detail.js
+++ sample/controllers/room_detail.js
@@ -1,5 +1,26 @@
 angular.module('ncy-sample')
-  .controller('RoomDetailCtrl', function($scope, $stateParams, rooms) {
+  .controller('RoomDetailCtrl', function($scope, $state, $stateParams, rooms) {
     $scope.rooms = rooms;
-    $scope.room = _.findWhere(rooms, {roomId: parseInt($stateParams.roomId)});
+
+    if($stateParams.roomId) {
+      $scope.room = _.findWhere(rooms, {roomId: parseInt($stateParams.roomId)});
+      if($scope.room) {
+        $scope.model = angular.copy($scope.room);
+      } else {
+        $state.go('^');
+      }
+
+    }
+
+    $scope.save = function() {
+      if($scope.model.roomId) {
+        angular.extend($scope.room, $scope.model);
+      } else {
+        var ids = _.map(rooms, function(room) { return room.roomId; });
+        $scope.model.roomId = _.max(ids) + 1;
+        rooms.push($scope.model);
+      }
+      $state.go('^');
+    }
+
   });
