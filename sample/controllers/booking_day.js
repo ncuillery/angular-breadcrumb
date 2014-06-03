95236916e00b19464a3dfe3584ef1b18da9ffb25
ncuillery <nicolas.cuillery@gmail.com>
2014-06-03 19:14:19 +0200

    fix(sample): prevent direct URL access to a day disabled in the datepicker
    Redirection from `booking.day` to `booking` if the date has no reservation.
    
    Closes: #17

--- sample/controllers/booking_day.js
+++ sample/controllers/booking_day.js
@@ -1,6 +1,10 @@
 angular.module('ncy-sample')
-  .controller('BookingDayCtrl', function($scope, $rootScope, $stateParams, rooms) {
+  .controller('BookingDayCtrl', function($scope, $rootScope, $state, $stateParams, rooms) {
     $rootScope.reservationDate = new Date($stateParams.year, $stateParams.month - 1, $stateParams.day);
+
+    if(!$scope.between($rootScope.reservationDate).length) {
+        $state.go('^');
+    }
 
     $scope.getRoom = function(id) {
         return _.findWhere(rooms, {roomId: parseInt(id)});
