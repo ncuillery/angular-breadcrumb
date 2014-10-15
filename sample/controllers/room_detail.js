2f9d564244f4b470aba42196e43710c18769d838
ncuillery <nicolas.cuillery@gmail.com>
2014-10-15 22:05:31 +0200

    chore(sample): Add an other way to access to the `room.detail` state
    The `room.detail` can be accessed via the `room` state and the
    `booking.day` state by clicking on the room number in the list of
    reservations.
    
    It demonstrates the ability to reference a parent state with url params (#46).

--- sample/controllers/room_detail.js
+++ sample/controllers/room_detail.js
@@ -1,6 +1,10 @@
 angular.module('ncy-sample')
   .controller('RoomDetailCtrl', function($scope, $state, $stateParams, rooms) {
     $scope.rooms = rooms;
+    if($stateParams.from) {
+        $scope.from = $stateParams.from.split('|')[0];
+        $scope.reservationDate = new Date(parseInt($stateParams.from.split('|')[1]));
+    }
 
     if($stateParams.roomId) {
       $scope.room = _.findWhere(rooms, {roomId: parseInt($stateParams.roomId)});
