ncuillery/angular-breadcrumb

commit 60ed0a58d41332edb9275acaca9b26d614b06bdf

chore(sample): Use the official repo for bootstrap

Gruntfile.js
@@ -125,7 +125,7 @@ module.exports = function (grunt) {
               'bower_components/angular/angular.js',
               'bower_components/angular-ui-router/release/angular-ui-router.js',
               'bower_components/angular-ui-bootstrap-bower/ui-bootstrap-tpls.js',
-              'bower_components/bootstrap.css/css/bootstrap.css',
+              'bower_components/bootstrap/docs/assets/css/bootstrap.css',
               'bower_components/underscore/underscore.js'
             ],
             dest: 'sample/asset/'

commit 6adebcd3c24ed54d3dd7af1f31f7a584a4189bb3

chore(sample): Copy the correct bootstrap.css when starting the sample
Regression after 60ed0a58d4

Gruntfile.js
@@ -125,7 +125,7 @@ module.exports = function (grunt) {
               'bower_components/angular/angular.js',
               'bower_components/angular-ui-router/release/angular-ui-router.js',
               'bower_components/angular-ui-bootstrap-bower/ui-bootstrap-tpls.js',
-              'bower_components/bootstrap/docs/assets/css/bootstrap.css',
+              'bower_components/bootstrap.css/css/bootstrap.css',
               'bower_components/underscore/underscore.js'
             ],
             dest: 'sample/asset/'